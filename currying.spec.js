641faab4db9c7e78dc7f9aca7973dcc81e2a5e87
Simon Kerr <simon.kerr@capitalone.com>
2017-04-06 15:59:32 +0100

    adding composition tests

--- currying.spec.js
+++ currying.spec.js
@@ -75,7 +75,108 @@ describe('Curry', () => {
 
     });
 
-    it('allows functions to be composed together', () => {
+    it('allows forking', () => {
+
+        /*
+        fork takes in 2 functions and an argument, uses that argument in each
+        function, then passes the result of those to lastly
+        */
+        const fork = _.curry((lastly, f, g, x) => lastly(f(x), g(x)));
+
+        /*
+        so in avg, the function gets the length of the array, gets the sum of the array
+        then divides the two
+         */
+        const avg = fork(_.divide, _.sum, _.length);
+        assert.equal(3, avg([1,2,3,4,5]));
+    });
+
+    describe('examples', () => {
+        it('make split work on an array of strings', () => {
+            const words = _.split(' ');
+
+            // map expects a function to use on each element, and an array.
+            // words produces an array
+            const sentences = _.map(words);
+
+            // should output array of arrays
+            const result = sentences(['a b c', 'd e f', 'g h i']);
+
+            assert.deepEqual([
+                ['a','b','c'],
+                ['d','e','f'],
+                ['g','h','i']
+            ], result);
+
+        });
+
+        it('refactor function to remove all arguments', () => {
+            const filterEven = xs => (
+                _.filter(x => x % 2 === 0, xs)
+            );
+            // curried function doesn't need the array passing in straight away
+            const filterEven2 = _.filter(x => x % 2 === 0);
+
+            const result1 = filterEven([1,2,3,4]);
+            const result2 = filterEven2([1,2,3,4])
+            assert.deepEqual([2,4], result1);
+            assert.deepEqual([2,4], result2);
+        });
+
+        it('refactor max to remove points', () => {
+            const _keepHighest = (x, y) => x >= y ? x : y;
+
+            const max = xs => (
+                _.reduce((acc, x) => (
+                    _keepHighest(acc, x)
+                ), -Infinity, xs)
+            );
+
+            // reduce takes 3 params - reduce function, starting value, array
+            const max2 = _.reduce((acc, x) => (
+                _keepHighest(acc, x)
+            ), -Infinity);
+
+            const result1 = max([1,5,3,200,-4]);
+            const result2 = max2([1,5,3,2,-4000,6,1,54]);
+
+            assert.equal(200, result1);
+            assert.equal(54, result2);
+
+        });
+
+        it('refactor array slice to be functional and curried', () => {
+            // //[1, 2, 3].slice(0, 2)
+            // should take a start and end
+            // const slice = (start, end) => _.slice(start, end);
+            const slice5Chars = _.slice(0,5);
+
+            assert.equal('Bobba', slice5Chars('BobbaFett'));
+
+        });
+        
+        it('extend slice to create "take" function', () => {
+            // take accepts a character limit and a string
+            const take = _.curry((charLimit, str) => _.slice(0, charLimit, str));
+
+            assert.equal('Sim', take(3)('Simon'));
+        });
+    });
+
+
+})
+
+describe('Compose', () => {
+    it('allows functions to be combined', () => {
+        const toUpperCase = x => x.toUpperCase();
+        const exclaim = x => `${x}!`;
+
+        const shout = _.compose(exclaim, toUpperCase);
+
+        assert.equal('HI THERE!', shout('hi there'));
+    });
+
+    it('example 2', () => {
         /**
          * this composed function splits words in a string
          * and returns an array of their lengths
@@ -120,7 +221,7 @@ describe('Curry', () => {
          remembering compose is evaluated right to left helps here.
          For each list item, author object is returned, and this is composed
          with the get function to get 'name' of that object
-        */
+         */
         // const names = _.compose(_.map(get('name')), _.map(get('author')));
 
         // function to say whether given string is an author
@@ -131,24 +232,45 @@ describe('Curry', () => {
         assert.isFalse(isAuthor('Blah Blah', articles));
     });
 
-    it('allows forking', () => {
+    it('works with pointfree programming', () => {
+        const snakeCase = _.compose(_.replace(/\s+/ig, '_'), _.toLower);
+        assert.equal('simon_kerr', snakeCase('Simon Kerr'));
+    });
 
-        /*
-        fork takes in 2 functions and an argument, uses that argument in each
-        function, then passes the result of those to lastly
-        */
-        const fork = _.curry((lastly, f, g, x) => lastly(f(x), g(x)));
+    it('pointfree example 2', () => {
+        // laid out like this, it works bottom to top
+        const initials = _.compose(
+            _.join('. '),
+            _.map(
+                _.compose(
+                    _.toUpper,
+                    _.head
+                )
+            ),
+            _.split(' ')
+        );
 
-        /*
-        so in avg, the function gets the length of the array, gets the sum of the array
-        then divides the two
-         */
-        const avg = fork(_.divide, _.sum, _.length);
-        assert.equal(3, avg([1,2,3,4,5]));
+        assert.equal('S. P. K', initials('simon phillip kerr'));
+    });
+
+    describe('debugging', () => {
+        it('use a simple "trace" function to debug compositions', () => {
+            const trace = _.curry((tag, x) => {
+                console.log(tag, x);
+                return x;
+            });
+
+            var dasherize = _.compose(_.join('-'), _.toLower, trace('after split'), _.split(' '), _.replace(/\s{2,}/ig, ' '));
+            //outputs 'after split' Array(5) - it was trying to call toLower on an array
+
+            dasherize = _.compose(_.join('-'), _.map(_.toLower), _.split(' '), _.replace(/\s{2,}/ig, ' '));
+            dasherize('The world is a vampire');
+        });
+
     });
 
 
-})
+});
 
 /**
  * Functors allow you to call map over any object
